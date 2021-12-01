21a4564c4e7e1bad415ca424ee6c94a083e643fb
yuncong yang <yuncong@pineapple.cs.columbia.edu>
2021-12-01 16:06:41 -0500

    completed

--- scripts/attack.py
+++ scripts/attack.py
@@ -24,8 +24,9 @@ from protonets.utils.yaml_loader import load_yaml
 from protonets.utils.arguments_parser import parse_arguments
 from protonets.utils.logging import get_logger
 from protonets.utils.time_measurement import measure_time
+from protonets.utils.metric import calculate_loss_metric
 
-def attack_pgd(attack, embedding_net, cls_head, config, data_query, emb_support, labels_query, labels_support, way, shot, episodes_per_batch, n_query, maxIter=3):
+def attack_pgd(attack, embedding_net, config, data_query, emb_support, labels_query, labels_support, way, episodes_per_batch, n_query):
     if not attack:
         return data_query
     if config['targeted']:
@@ -47,10 +48,9 @@ def attack_pgd(attack, embedding_net, cls_head, config, data_query, emb_support,
         with torch.enable_grad():
             emb_query_adv = embedding_net.encoder(x.reshape([-1] + list(x.shape[-3:]))).reshape(episodes_per_batch, n_query, -1)
 
-            logits = cls_head(emb_query_adv, emb_support, labels_support, way, shot)
-
-            logits = logits.view(logits.size()[0]*logits.size()[1],logits.size()[2])
-            loss = F.cross_entropy(logits, new_labels_query, size_average=False)
+            logits = calculate_loss_metric(way, 1, labels_support, emb_query_adv, emb_support)
+            
+            loss = logits['loss']
         grad = torch.autograd.grad(loss, [x])[0]
         if config['targeted']:
             x = x.detach() - config['step_size']*torch.sign(grad.detach())
@@ -60,36 +60,36 @@ def attack_pgd(attack, embedding_net, cls_head, config, data_query, emb_support,
         x = torch.clamp(x, 0.0, 1.0)
     return x
 
-def AttackPGDFeatureExtractor(attack, embedding_net, cls_head, config, data_query, labels_query, ways=64, maxIter = 3):
-    if not attack:
-        return data_query
-    if config['targeted']:
-        new_labels_query = torch.zeros_like(labels_query)
-        for i in range(int(labels_query.size()[0])):
-            while True:
-                new_labels_query[i] = random.randint(0,ways-1)
-                if new_labels_query[i] != labels_query[i]:
-                    break
-    else:
-        new_labels_query = labels_query
+# def AttackPGDFeatureExtractor(attack, embedding_net, cls_head, config, data_query, labels_query, ways=64, maxIter = 3):
+#     if not attack:
+#         return data_query
+#     if config['targeted']:
+#         new_labels_query = torch.zeros_like(labels_query)
+#         for i in range(int(labels_query.size()[0])):
+#             while True:
+#                 new_labels_query[i] = random.randint(0,ways-1)
+#                 if new_labels_query[i] != labels_query[i]:
+#                     break
+#     else:
+#         new_labels_query = labels_query
 
-    x = data_query.detach()
-    if config['random_init']:
-        x = x + torch.zeros_like(x).uniform_(-config['epsilon'], config['epsilon'])
-    for i in range(config['num_steps']):
-        x.requires_grad_()
-        with torch.enable_grad():
-            emb_query_adv = embedding_net(X)
-            logits = cls_head(emb_query_adv)
-            loss = F.cross_entropy(logits, new_labels_query)
-        grad = torch.autograd.grad(loss, [x])[0]
-        if config['targeted']:
-            x = x.detach() - config['step_size']*torch.sign(grad.detach())
-        else:
-            x = x.detach() + config['step_size']*torch.sign(grad.detach())
-        x = torch.min(torch.max(x, data_query - config['epsilon']), data_query + config['epsilon'])
-        x = torch.clamp(x, 0.0, 1.0)
-    return x
+#     x = data_query.detach()
+#     if config['random_init']:
+#         x = x + torch.zeros_like(x).uniform_(-config['epsilon'], config['epsilon'])
+#     for i in range(config['num_steps']):
+#         x.requires_grad_()
+#         with torch.enable_grad():
+#             emb_query_adv = embedding_net(X)
+#             logits = cls_head(emb_query_adv)
+#             loss = F.cross_entropy(logits, new_labels_query)
+#         grad = torch.autograd.grad(loss, [x])[0]
+#         if config['targeted']:
+#             x = x.detach() - config['step_size']*torch.sign(grad.detach())
+#         else:
+#             x = x.detach() + config['step_size']*torch.sign(grad.detach())
+#         x = torch.min(torch.max(x, data_query - config['epsilon']), data_query + config['epsilon'])
+#         x = torch.clamp(x, 0.0, 1.0)
+#     return x
     
 # 
 # config = {
@@ -101,6 +101,6 @@ def AttackPGDFeatureExtractor(attack, embedding_net, cls_head, config, data_quer
 # }
 # 
 
-model = load_model('vanilla', (3, 84, 84), 64, 64)
-z_proto = torch.rand(60, 64)
-query_data = torch.rand(1, 64)
+# model = load_model('vanilla', (3, 84, 84), 64, 64)
+# z_proto = torch.rand(60, 64)
+# query_data = torch.rand(1, 64)
